eb6ade24f4a4064dd070836376db609c408f8ed6
Pradeep Elankumaran <pradeepe@gmail.com>
2010-01-10 09:19:35 -0800

    fleshed out the routing dsl some more

--- lib/llama.rb
+++ lib/llama.rb
@@ -1,6 +1,23 @@
+require 'rubygems'
+require 'eventmachine'
 
 module Llama
-  class Message
+  def self.start(&block)
+    EventMachine::run do
+      puts "Starting Llama..."
+      block.call
+    end
+  end 
+  
+  module Message
+    class Base
+    end
+
+    class JSONMessage < Base
+    end
+
+    class XmlMessage < Base
+    end
   end
 
   class Component
@@ -8,45 +25,117 @@ module Llama
 
   module Producer
     class Base < Component
+      def produce(*args)
+        raise "Subclass #{self} and define this method"
+      end
     end
 
     class DiskFile < Base 
+      def initialize(filename)
+        @filename = filename
+      end
+
+      def produce
+        puts "Producing from #{@filename}" 
+        return "HELLO"
+      end
     end
   end
 
   module Consumer
-    class Base < Llama::Component
+    class Base < Component
+      def consume(*args) 
+        raise "Subclass #{self} and define this method"
+      end
+    end
+
+    class Stdout < Base
+      def consume(message)
+        puts message
+      end
     end
   end
 
   class Filter
   end
 
-  class Router
-    class ComponentNotFoundException < StandardError; end
-    def setup_routes(&block)
-      raise "Subclass this"
+  module Routing 
+    class Route
+      class ComponentNotFoundException < StandardError; end
+      class ProducerComponentNotAllowed < StandardError; end
+
+      def initialize(*args)
+        @chain = []
+      end
+
+      def from(component)
+        raise ComponentNotFoundException unless component.kind_of?(Llama::Component) 
+        puts "From #{component}"
+        @chain.push(component)
+        self
+      end
+
+      def to(component)
+        raise ComponentNotFoundException unless component.kind_of?(Llama::Component) 
+        raise ProducerComponentNotAllowed if component.kind_of?(Llama::Producer::Base)
+        puts "To #{component}"
+        @chain.push(component)
+        self 
+      end
+
+      def run
+        EventMachine::spawn {
+          puts "Running #{self}"
+        }
+      end
+
+      def to_s
+        @chain.to_s
+      end
+
+      def inspect
+        @chain.inspect
+      end
     end
 
-    def from(component)
-      raise ComponentNotFoundException unless component.kind_of?(Llama::Component) 
-      return self
-    end
+    class Router
+      def initialize
+        puts "Initializing Router..."
+        @routes ||= []
+      end
 
-    def to(component)
-      raise ComponentNotFoundException unless component.kind_of?(Llama::Component) 
-      return self
-    end
+      def setup_routes(&block)
+        raise "Subclass this"
+      end
 
-    def self.start
+      def from(component)
+        return Route.new.from(component) 
+      end
+
+      def add(built_route)
+        @routes.push(built_route)
+      end
+
+      def run
+        @routes.each{|r| r.run}
+      end
+
+      def self.start
+        router = new
+        router.setup_routes
+        router.run
+      end
     end
   end
 end
 
-class MyRouter < Llama::Router
-  def setup_routes
-    from(Llama::Components::DiskFile.new("test.data")).to($STDOUT)
+Llama::start do
+  class MyRouter < Llama::Routing::Router
+    def setup_routes
+      add from(Llama::Producer::DiskFile.new("test.data")).
+          to(Llama::Consumer::Stdout.new)
+    end
   end
-end
 
-MyRouter.start
+  MyRouter.start
+end
